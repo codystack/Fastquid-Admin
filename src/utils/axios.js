3201bfadf422f3ae5d6871c2816c6b8570f0d30d
Styneman <stanleynyekpeye@gmail.com>
2024-04-05 09:44:40 +0100

    Newest commit

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { APP_KEY } from "config";
 
 
-const BASE_URL = "https://fast-quid-api-service.vercel.app"; //  "http://192.168.1.21:8080"; // 
+const BASE_URL = "https://fast-quid-api-service.vercel.app"; 
 
 const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api`,
